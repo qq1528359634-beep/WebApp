8d2cf43b56ca01ec25e1f88023660c405463d6de
KANSEI CHIN <qq1528359634@gmail.com>
2026-03-10 23:21:39 +0900

    Configure HttpClient and implement WebApiExecuter

--- Program.cs
+++ Program.cs
@@ -4,12 +4,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-//注册HttpClient服务，命名为ShirtApi，设置基础地址和默认请求头
-builder.Services.AddHttpClient("ShirtsApi", client =>
+//配置客户端，命名为ShirtApi，设置根地址和默认请求头
+builder.Services.AddHttpClient("ShirtsAPi",client =>
 {
-    client.BaseAddress=new Uri("https://localhost:7025/api/");
-    client.DefaultRequestHeaders.Add("Accept", "application/json");
+    client.BaseAddress = new Uri("https://localhost:7025/api");
+    client.DefaultRequestHeaders.Add("Accept","Appliaction/Json");
 });
+
+
 //Transient瞬时生命周期：每次请求都会创建一个新的实例，适用于轻量级、无状态的服务。
 //将服务和服务的实现类型注册到依赖注入容器中，
 //这样在需要使用IWebApiExecuter的地方，框架会自动提供一个WebApiExecuter实例。
--- date/WebApiExecuter.cs
+++ date/WebApiExecuter.cs
@@ -1,22 +1,24 @@
 ﻿namespace WebApp.date
 {
-    public class WebApiExecuter : IWebApiExecuter
+    public class WebApiExecuter:IWebApiExecuter
     {
-        private const string apiName = "ShirtsApi";
+       public const string ApiName= "ShirtsApi";
         private readonly IHttpClientFactory httpClientFactory;
 
         public WebApiExecuter(IHttpClientFactory httpClientFactory)
         {
             this.httpClientFactory = httpClientFactory;
         }
+        public async Task<T?> InvokeGet<T>(string relativeUrl )
+        {
+            var httpClient=this.httpClientFactory.CreateClient(ApiName);
+            return await httpClient.GetFromJsonAsync<T>(relativeUrl);
 
-        public async Task<T?> InvokeGet<T>(string relativerUrl)
-        {   //获取已经注册的HTTP客户端实例，apiName是注册时指定的名称
-            var httpClient = httpClientFactory.CreateClient(apiName);
-            //GetFromJsonAsync发送GET请求到指定的相对URL，并将响应内容反序列化为类型T的对象。
-            //GetFromJsonAsync<T>方法会将HTTP响应内容反序列化为指定类型的对象
-            //如果响应内容无法被正确反序列化，或者HTTP请求失败，则会返回null。
-            return await httpClient.GetFromJsonAsync<T>(relativerUrl);
-        }
+        }  
+
     }
 }
+//获取已经注册的HTTP客户端实例，apiName是注册时指定的名称
+//GetFromJsonAsync发送GET请求到指定的相对URL，并将响应内容反序列化为类型T的对象。
+//GetFromJsonAsync<T>方法会将HTTP响应内容反序列化为指定类型的对象
+//如果响应内容无法被正确反序列化，或者HTTP请求失败，则会返回null。
